789651c7ea9ee484bbab2e887e8e88699bf0370b
c1316363 David Jeremias Souza do Carmo <c1316363@interno.bb.com.br>
2020-08-17 16:21:23 -0300

    first commit

---
+++ dflex-delivery-backend/src/main/java/com/u2d/projeto/DflexDeliveryApplication.java
@@ -0,0 +1,12 @@
+package com.u2d.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DflexDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DflexDeliveryApplication.class, args);
+	}
+}
